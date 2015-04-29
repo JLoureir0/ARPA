5545830688accf511e8d6d355bf9601f5e2ac75c
João Loureiro <joao.a.g.loureiro@gmail.com>
2015-04-29 22:31:45 +0100

    Added grettings to the root url (/) and defined charset to utf-8, tests included

--- server/app.js
+++ server/app.js
@@ -3,11 +3,22 @@ var server  = restify.createServer({ name: 'ARPA' });
 
 var logger = require('restify-logger');
 
+var greetings = 'Connect with ARPA';
+
 server.use(restify.fullResponse());
 server.use(restify.bodyParser());
 
 server.use(logger('dev'));
 
+server.use(function(req, res, next) {
+  res.charSet('utf-8');
+  next();
+});
+
 server.listen(3000, function() {
   console.log(server.name + ' listening at ' + server.url);
+});
+
+server.get('/', function(req, res) {
+  res.send({ data: greetings });
 });
--- server/test/serverSpec.js
+++ server/test/serverSpec.js
@@ -5,15 +5,25 @@ var client = restify.createJsonClient({
   url: 'http://localhost:3000'
 });
 
+var greetings = 'Connect with ARPA';
+
 describe('Server', function() {
   it('should be running', function(done) {
     client.get('/', function(err, req, res, obj) {
+      expect(res.statusCode).to.be.equal(200);
+      expect(obj.data).to.be.deep.equal(greetings);
       done();
     });
   });
   it('should return 404 if the url is invalid', function(done) {
     client.get('/abc', function(err, req, res, obj) {
       expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+  it('should have Content-Type: application/json; charset=utf-8', function(done) {
+    client.get('/', function(err, req, res, obj) {
+      expect(res.headers['content-type']).to.be.equal('application/json; charset=utf-8');
       done();
     });
   });
