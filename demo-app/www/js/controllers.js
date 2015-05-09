36eefe4b3641d18c4375ebe029c68fde9a031528
João Loureiro <joao.a.g.loureiro@gmail.com>
2015-05-09 22:31:03 +0100

    inSessionID is no longer hardcoded, it is retrieved from the servers if no session exits or the session has expired

--- demo-app/www/js/controllers.js
+++ demo-app/www/js/controllers.js
@@ -2,17 +2,39 @@ angular.module('controllers', [])
 
 .controller('SearchCtrl', function($scope, $http) {
   $scope.payload = {
-    inSessionID: "20dda1cb28a44b7f93e359e8f3b5f1c2",
     inSortOrder: "ASC",
     inPageNumber: 1,
     inPageSize: 50
   };
 
   $scope.search = function() {
+    search_product();
+  };
+
+  function search_product() {
     var url = 'https://m.continente.pt/MRS.Web/Proxy.ashx?Method=/BSProductCatalog/SearchProduct';
     $http.post(url, $scope.payload, { headers: { 'Content-Type': 'application/json' } })
       .then(function(response) {
-        $scope.results = response.data.SearchProductResult.Result.ItemList;
-      });
-  };
+        var result = response.data.SearchProductResult;
+        console.log(JSON.stringify(response));
+        if(result.Code === 0)
+          $scope.results = result.Result.ItemList;
+        else if(result.Code === 1)
+          $scope.results = [{ Name: result.Message }];
+        else if(result.Code === -88888 || result.Code === -99999) {
+          anonymous_authentication(search_product);
+        }
+    });
+  }
+
+  function anonymous_authentication(done) {
+    var url = 'https://m.continente.pt/MRS.Web/Proxy.ashx?Method=/BSLogin/AuthenticateAnonymous';
+    $http.post(url, { inLanguage:"pt" }, { headers: { 'Content-Type': 'application/json' } })
+      .then(function(response) {
+        console.log(response);
+        if(response.data.AuthenticateAnonymousResult.Code === 0)
+          $scope.payload.inSessionID = response.data.AuthenticateAnonymousResult.Result;
+        done();
+    });
+  }
 });
