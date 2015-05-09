3d597f4e929259bc8621ea7bc447b45f1560dabf
João Loureiro <joao.a.g.loureiro@gmail.com>
2015-05-09 16:25:48 +0100

    Search is already retrieving data from Continente servers

--- demo-app/www/js/controllers.js
+++ demo-app/www/js/controllers.js
@@ -1,7 +1,18 @@
 angular.module('controllers', [])
 
-.controller('SearchCtrl', function($scope) {
+.controller('SearchCtrl', function($scope, $http) {
+  $scope.payload = {
+    inSessionID: "20dda1cb28a44b7f93e359e8f3b5f1c2",
+    inSortOrder: "ASC",
+    inPageNumber: 1,
+    inPageSize: 50
+  };
+
   $scope.search = function() {
-    alert('SEARCH');
+    var url = 'https://m.continente.pt/MRS.Web/Proxy.ashx?Method=/BSProductCatalog/SearchProduct';
+    $http.post(url, $scope.payload, { headers: { 'Content-Type': 'application/json' } })
+      .then(function(response) {
+        $scope.results = response.data.SearchProductResult.Result.ItemList;
+      });
   };
 });
