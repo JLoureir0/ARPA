0cabbe5a8b68d115085987e61d867f784746f927
João Loureiro <joao.a.g.loureiro@gmail.com>
2015-05-09 15:39:15 +0100

    Moved ngEnter directive to a module called directives

--- demo-app/www/js/controllers.js
+++ demo-app/www/js/controllers.js
@@ -4,20 +4,4 @@ angular.module('controllers', [])
   $scope.search = function() {
     alert('SEARCH');
   };
-})
-
-.directive('ngEnter', function () {
-  return function (scope, element, attrs) {
-    element.bind("keydown keypress", function (event) {
-      if(event.which === 13) {
-        var phase = scope.$root.$$phase;
-        if(phase != '$apply' && phase != '$digest')
-          scope.$apply(function (){
-            scope.$eval(attrs.ngEnter);
-          });
-
-        event.preventDefault();
-      }
-    });
-  };
 });
---
+++ demo-app/www/js/directives.js
@@ -0,0 +1,17 @@
+angular.module('directives', [])
+
+.directive('ngEnter', function () {
+  return function (scope, element, attrs) {
+    element.bind("keydown keypress", function (event) {
+      if(event.which === 13) {
+        var phase = scope.$root.$$phase;
+        if(phase != '$apply' && phase != '$digest')
+          scope.$apply(function (){
+            scope.$eval(attrs.ngEnter);
+          });
+
+        event.preventDefault();
+      }
+    });
+  };
+});
