56742d43a795ec4a48c44afaf5f073162a158c07
fabio-silva <ffjs1993@gmail.com>
2015-05-12 23:45:46 +0100

    Facebook on mobile

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -5,25 +5,36 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('arpa', ['ionic', 'arpa.controllers', 'arpa.services'])
+angular.module('arpa', ['ionic', 'arpa.controllers', 'arpa.services', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
+  if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  }
+  if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
+
   });
 })
-    .config(function($ionicConfigProvider) {if(!ionic.Platform.isIOS())$ionicConfigProvider.scrolling.jsScrolling(false);})
+
+.config(function($cordovaFacebookProvider){
+
+  if(!window.cordova){
+    var appID = 367156356826931;
+    var version = "v2.0";
+    $cordovaFacebookProvider.browserInit(appID, version);
+  }
+  
+})
+
+.config(function($ionicConfigProvider) {if(!ionic.Platform.isIOS())$ionicConfigProvider.scrolling.jsScrolling(false);})
 
 .config(function($stateProvider, $urlRouterProvider) {
-  openFB.init({appId:'367156356826931'}, ionic.Platform.isWebView());
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
@@ -31,34 +42,34 @@ angular.module('arpa', ['ionic', 'arpa.controllers', 'arpa.services'])
   $stateProvider
 
   // setup an abstract state for the tabs directive
-    .state('tab', {
+  .state('tab', {
     url: "/tab",
     abstract: true,
     templateUrl: "templates/tabs.html",
-	controller: 'MainCtrl'
+    controller: 'MainCtrl'
   })
 
   // Each tab has its own nav history stack:
 
-      .state('tab.allergens', {
-        url: '/allergens',
-        views: {
-          'tab-allergens': {
-            templateUrl: 'templates/tab-allergens.html',
-            controller: 'AllergensCtrl'
-          }
-        }
-      })
-	  
-      .state('tab.definitions', {
-        url: '/definitions',
-        views: {
-          'tab-definitions': {
-            templateUrl: 'templates/tab-definitions.html',
-            controller: 'DefinitionsCtrl'
-          }
-        }
-      })
+  .state('tab.allergens', {
+    url: '/allergens',
+    views: {
+      'tab-allergens': {
+        templateUrl: 'templates/tab-allergens.html',
+        controller: 'AllergensCtrl'
+      }
+    }
+  })
+
+  .state('tab.definitions', {
+    url: '/definitions',
+    views: {
+      'tab-definitions': {
+        templateUrl: 'templates/tab-definitions.html',
+        controller: 'DefinitionsCtrl'
+      }
+    }
+  })
 
   .state('tab.dash', {
     url: '/dash',
@@ -71,23 +82,23 @@ angular.module('arpa', ['ionic', 'arpa.controllers', 'arpa.services'])
   })
 
   .state('tab.applications', {
-      url: '/applications',
-      views: {
-        'tab-applications': {
-          templateUrl: 'templates/tab-applications.html',
-          controller: 'AppsCtrl'
-        }
+    url: '/applications',
+    views: {
+      'tab-applications': {
+        templateUrl: 'templates/tab-applications.html',
+        controller: 'AppsCtrl'
       }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
+    }
+  })
+  .state('tab.chat-detail', {
+    url: '/chats/:chatId',
+    views: {
+      'tab-chats': {
+        templateUrl: 'templates/chat-detail.html',
+        controller: 'ChatDetailCtrl'
       }
-    })
+    }
+  })
 
   .state('tab.account', {
     url: '/account',
