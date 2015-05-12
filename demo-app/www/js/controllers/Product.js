85e6a5e752ded5092e1feb73048bde501cc25c5e
João Loureiro <joao.a.g.loureiro@gmail.com>
2015-05-12 02:58:19 +0100

    Moved the logic of hide the loading screen and the pop up alert from the factory to the controllers

--- demo-app/www/js/controllers/Product.js
+++ demo-app/www/js/controllers/Product.js
@@ -2,7 +2,7 @@ angular.module('demo-app')
 
 .controller('ProductController', SearchController);
 
-function SearchController($ionicLoading, $stateParams, productFactory) {
+function SearchController($ionicLoading, $ionicPopup, $state, $stateParams, productFactory) {
   var vm = this;
 
   $ionicLoading.show({ delay: 250 });
@@ -12,8 +12,17 @@ function SearchController($ionicLoading, $stateParams, productFactory) {
   function getProductInfo() {
     return productFactory.getProductInfo($stateParams.productID)
     .then(function(data) {
+      $ionicLoading.hide();
       vm.product = data;
       return vm.product;
+    })
+    .catch(function(reason) {
+      $ionicLoading.hide();
+      $ionicPopup.alert({
+        title   : 'Check your internet connection!',
+        buttons : [{ text: 'Ok', type: 'button-assertive' }]
+      });
+      $state.go('search');
     });
   }
 }
