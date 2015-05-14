4354b3729a04beabd7ed713880c19dc9fd1f0fa7
Jorge Lima <ei10102@fe.up.pt>
2015-05-14 15:34:30 +0100

    fixed cordova undefined with ionic.platform.ready

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -5,116 +5,114 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-<<<<<<< HEAD
-angular.module('arpa', ['ionic', 'arpa.controllers', 'arpa.services', 'ngAnimate'])
-=======
-angular.module('arpa', ['ionic', 'arpa.controllers', 'arpa.services', 'ngCordova'])
->>>>>>> 56742d43a795ec4a48c44afaf5f073162a158c07
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-  if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-  }
-  if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
+angular.module('arpa', ['ionic', 'arpa.controllers', 'arpa.services', 'ngCordova', 'ngAnimate'])
 
-  });
-})
+    .run(function($ionicPlatform) {
+      $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+          // org.apache.cordova.statusbar required
+          StatusBar.styleLightContent();
+        }
 
-.config(function($cordovaFacebookProvider){
+      });
+    })
 
-  if(!window.cordova){
-    var appID = 367156356826931;
-    var version = "v2.0";
-    $cordovaFacebookProvider.browserInit(appID, version);
-  }
-  
-})
+    .config(function($cordovaFacebookProvider){
 
-.config(function($ionicConfigProvider) {if(!ionic.Platform.isIOS())$ionicConfigProvider.scrolling.jsScrolling(false);})
+      ionic.Platform.ready(function () {
+        var appID = 367156356826931;
+        var version = "v2.0";
+        $cordovaFacebookProvider.browserInit(appID, version);
+      });
 
-.config(function($stateProvider, $urlRouterProvider) {
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
 
-  // setup an abstract state for the tabs directive
-  .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html",
-    controller: 'MainCtrl'
-  })
+    })
 
-  // Each tab has its own nav history stack:
+    .config(function($ionicConfigProvider) {if(!ionic.Platform.isIOS())$ionicConfigProvider.scrolling.jsScrolling(false);})
 
-  .state('tab.allergens', {
-    url: '/allergens',
-    views: {
-      'tab-allergens': {
-        templateUrl: 'templates/tab-allergens.html',
-        controller: 'AllergensCtrl'
-      }
-    }
-  })
+    .config(function($stateProvider, $urlRouterProvider) {
+      // Ionic uses AngularUI Router which uses the concept of states
+      // Learn more here: https://github.com/angular-ui/ui-router
+      // Set up the various states which the app can be in.
+      // Each state's controller can be found in controllers.js
+      $stateProvider
 
-  .state('tab.definitions', {
-    url: '/definitions',
-    views: {
-      'tab-definitions': {
-        templateUrl: 'templates/tab-definitions.html',
-        controller: 'DefinitionsCtrl'
-      }
-    }
-  })
+        // setup an abstract state for the tabs directive
+          .state('tab', {
+            url: "/tab",
+            abstract: true,
+            templateUrl: "templates/tabs.html",
+            controller: 'MainCtrl'
+          })
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
+        // Each tab has its own nav history stack:
 
-  .state('tab.applications', {
-    url: '/applications',
-    views: {
-      'tab-applications': {
-        templateUrl: 'templates/tab-applications.html',
-        controller: 'AppsCtrl'
-      }
-    }
-  })
-  .state('tab.chat-detail', {
-    url: '/chats/:chatId',
-    views: {
-      'tab-chats': {
-        templateUrl: 'templates/chat-detail.html',
-        controller: 'ChatDetailCtrl'
-      }
-    }
-  })
+          .state('tab.allergens', {
+            url: '/allergens',
+            views: {
+              'tab-allergens': {
+                templateUrl: 'templates/tab-allergens.html',
+                controller: 'AllergensCtrl'
+              }
+            }
+          })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+          .state('tab.definitions', {
+            url: '/definitions',
+            views: {
+              'tab-definitions': {
+                templateUrl: 'templates/tab-definitions.html',
+                controller: 'DefinitionsCtrl'
+              }
+            }
+          })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/allergens');
+          .state('tab.dash', {
+            url: '/dash',
+            views: {
+              'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+              }
+            }
+          })
 
-});
+          .state('tab.applications', {
+            url: '/applications',
+            views: {
+              'tab-applications': {
+                templateUrl: 'templates/tab-applications.html',
+                controller: 'AppsCtrl'
+              }
+            }
+          })
+          .state('tab.chat-detail', {
+            url: '/chats/:chatId',
+            views: {
+              'tab-chats': {
+                templateUrl: 'templates/chat-detail.html',
+                controller: 'ChatDetailCtrl'
+              }
+            }
+          })
+
+          .state('tab.account', {
+            url: '/account',
+            views: {
+              'tab-account': {
+                templateUrl: 'templates/tab-account.html',
+                controller: 'AccountCtrl'
+              }
+            }
+          });
+
+      // if none of the above states are matched, use this as the fallback
+      $urlRouterProvider.otherwise('/tab/allergens');
+
+    });
