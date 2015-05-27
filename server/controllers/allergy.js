b19cdbadc2eecaddbef1332647e7410fa82ace6e
fabio-silva <ffjs1993@gmail.com>
2015-05-27 03:49:04 +0100

    Change frontend according to new server

--- server/controllers/allergy.js
+++ server/controllers/allergy.js
@@ -1,22 +1,5 @@
 var restify         = require('restify');
-var allergies_model = require('../models/newAllergies.js');
-
-//TODO Add all the allergies
-var valid_allergies = 
-[
-'lacteos', 
-'gluten', 
-'amendoins', 
-'ovos', 
-'marisco', 
-'moluscos',
-'mostarda',
-'peixe',
-'sesamo',
-'so2',
-'soja',
-'tremocos' ];
-
+var allergies_model = require('../models/allergies.js');
 
 exports.handle_params = function(req, res, next) {
   if(req.params.deviceId){
@@ -50,7 +33,7 @@ exports.handle_params = function(req, res, next) {
 
 exports.new_user = function(req, res){
   var user = {};
-  allergies_model.save(req.params[0], req.params.fbId, req.params.deviceId, req.params.intolerant, req.params.allergic, user, function(err, result){
+  allergies_model.save(req.params.appId, req.params.fbId, req.params.deviceId, req.params.intolerant, req.params.allergic, user, function(err, result){
     if(result){
       res.send(200, {data: user.id});
     } else{
@@ -87,47 +70,3 @@ exports.update_allergies = function(req, res) {
       });
   });
 };
-
-exports.delete_allergies = function(req, res) {
-  allergies_model.get(req.params[0], function(err, result) {
-    if(result.rows.length === 0)
-      res.send(404);
-    else
-      allergies_model.delete(req.params[0], function(err, result) {
-        if(result.rowCount === 1)
-          res.send(200);
-        else
-          res.send(404);
-      });
-  });
-};
-
-function verify_allergies_attributes(allergies, next) {
-  if(allergies.intolerant)
-    parse_intolerant(allergies.intolerant, next);
-  if(allergies.allergic)
-    parse_allergic(allergies.allergic, next);
-}
-
-function parse_intolerant(intolerant, next) {
-  var parsed = JSON.parse(intolerant);
-  if(parsed.constructor !== Array)
-    return next(new restify.InvalidArgumentError('intolerant must be an array'));
-
-  parse_valid_allergies(parsed, 'intolerant', next);
-}
-
-function parse_allergic(allergic, next) {
-  var parsed = JSON.parse(allergic);
-  console.log("PARSED: " + allergic);
-  if(parsed.constructor !== Array)
-    return next(new restify.InvalidArgumentError('allergic must be an array'));
-  parse_valid_allergies(parsed, 'allergic', next);
-}
-
-function parse_valid_allergies(array, type, next) {
-  array.forEach(function(element) {
-    if(valid_allergies.indexOf(element) === -1)
-      return next(new restify.InvalidArgumentError(type + ' must be an array with valid values'));
-  });
-}
--- server/routes/allergy.js
+++ server/routes/allergy.js
@@ -2,7 +2,7 @@ var allergy_controller = require('../controllers/allergy.js');
 
 exports.define_routes = function(server) {
   server.get(/^\/allergies\/(.+)/, allergy_controller.get_allergies);
-  server.put(/^\/allergies\/(.+)/, allergy_controller.handle_params, allergy_controller.update_allergies);
-  server.del(/^\/allergies\/(.+)/, allergy_controller.delete_allergies);
-  server.post(/^\/allergies\/(.+)/, allergy_controller.handle_params, allergy_controller.new_user);
+  //server.put(/^\/allergies\/(.+)/, allergy_controller.handle_params, allergy_controller.update_allergies);
+  //server.del(/^\/allergies\/(.+)/, allergy_controller.delete_allergies);
+  server.post(/^\/allergies\//, allergy_controller.handle_params, allergy_controller.new_user);
 };
