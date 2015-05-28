7e2fb09e01ee645dcc5512bec0dca3b01f2052a0
fabio-silva <ffjs1993@gmail.com>
2015-05-28 02:23:50 +0100

    GET allergens by deviceID

--- server/routes/allergy.js
+++ server/routes/allergy.js
@@ -1,8 +1,7 @@
 var allergy_controller = require('../controllers/allergy.js');
 
 exports.define_routes = function(server) {
-  server.get(/^\/allergies\/(.+)/, allergy_controller.get_allergies);
-  //server.put(/^\/allergies\/(.+)/, allergy_controller.handle_params, allergy_controller.update_allergies);
-  //server.del(/^\/allergies\/(.+)/, allergy_controller.delete_allergies);
+	server.get(/^\/allergies\/device\/(.+)/, allergy_controller.get_allergies_by_device);
+	server.get(/^\/allergies\/(.+)/, allergy_controller.get_allergies);
   server.post(/^\/allergies\//, allergy_controller.handle_params, allergy_controller.new_user);
 };
